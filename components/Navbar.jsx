8d7839ac2664f0b6669cac387f47a6a923f01236
Lindrit <lindritsulaj2.0@gmail.com>
2023-03-08 18:28:45 +0100

    Navbar completed

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -19,6 +19,52 @@ const Navbar = () => {
                 <span>Digio</span>
               </h1>
               <NavLink
+                title="Company"
+                hoverComponent={
+                  <div className='flex gap-8 px-5 py-4 h-auto'>
+                    <div className='w-[170px]'>
+                      <h3 className={`text-base font-medium text-neutral-800 ${poppins.className}`}>
+                        <span>About</span>
+                        <i className="fa-solid fa-arrow-right-long ml-3 text-green-600"></i>
+                      </h3>
+                      <hr className='mt-3 border-green-300' />
+                      <ul className='pt-4 pb-2 px-1 flex flex-col gap-y-3 text-neutral-700'>
+                        <li><a href="">Who We Are</a></li>
+                        <li><a href="">Customer Stories</a></li>
+                        <li><a href="">Blog</a></li>
+                        <li><a href="">Careers</a></li>
+                        <li><a href="">Leadership</a></li>
+                        <li><a href="">Investors</a></li>
+                      </ul>
+                    </div>
+                    <div className='w-[170px]'>
+                      <h3 className={`text-base font-medium text-neutral-800 ${poppins.className}`}>
+                        <span>Contact</span>
+                        <i className="fa-solid fa-arrow-right-long ml-3 text-green-600"></i>
+                      </h3>
+                      <hr className='mt-3 border-green-300' />
+                      <ul className='pt-4 pb-2 px-1 flex flex-col gap-y-3 text-neutral-700'>
+                        <li><a href="">Consulting</a></li>
+                        <li><a href="">Training</a></li>
+                        <li><a href="">Customer Support</a></li>
+                        <li><a href="">Customer Success</a></li>
+                      </ul>
+                    </div>
+                    <div className='w-[170px]'>
+                      <h3 className={`text-base font-medium text-neutral-800 ${poppins.className}`}>
+                        <span>Partnerships</span>
+                        <i className="fa-solid fa-arrow-right-long ml-3 text-green-600"></i>
+                      </h3>
+                      <hr className='mt-3 border-green-300' />
+                      <ul className='pt-4 pb-2 px-1 flex flex-col gap-y-3 text-neutral-700'>
+                        <li><a href="">Become a Partner</a></li>
+                        <li><a href="">Find a Partner</a></li>
+                        <li><a href="">Digio for Startups</a></li>
+                      </ul>
+                    </div>
+                  </div>
+                } />
+              <NavLink
                 title="Services"
                 hoverComponent={
                   <div className='flex gap-8 px-5 py-4 h-auto'>
@@ -47,21 +93,39 @@ const Navbar = () => {
                         <li><a href="">Maintenance</a></li>
                       </ul>
                     </div>
-                    <div>
-                      <ul>
-                        <li><a href=""></a></li>
-                        <li><a href=""></a></li>
-                        <li><a href=""></a></li>
-                        <li><a href=""></a></li>
-                      </ul>
-                    </div>
                   </div>} />
               <NavLink
                 title="Solutions"
-                hoverComponent={<p>Solutions Card</p>} />
-              <NavLink
-                title="Company"
-                hoverComponent={<p>Company Card</p>} />
+                hoverComponent={
+                  <div className='flex gap-8 px-5 py-4 h-auto'>
+                    <div className='w-[200px]'>
+                      <h3 className={`text-base font-medium text-neutral-800 ${poppins.className}`}>
+                        <span>By Industry</span>
+                        <i className="fa-solid fa-arrow-right-long ml-3 text-green-600"></i>
+                      </h3>
+                      <hr className='mt-3 border-green-300' />
+                      <ul className='pt-4 pb-2 px-1 flex flex-col gap-y-3 text-neutral-700'>
+                        <li><a href="">Healthcare</a></li>
+                        <li><a href="">Retail</a></li>
+                        <li><a href="">Manufacturing</a></li>
+                        <li><a href="">All industries</a></li>
+                      </ul>
+                    </div>
+                    <div className='w-[200px]'>
+                      <h3 className={`text-base font-medium text-neutral-800 ${poppins.className}`}>
+                        <span>By Use Case</span>
+                        <i className="fa-solid fa-arrow-right-long ml-3 text-green-600"></i>
+                      </h3>
+                      <hr className='mt-3 border-green-300' />
+                      <ul className='pt-4 pb-2 px-1 flex flex-col gap-y-3 text-neutral-700'>
+                        <li><a href="">Analytics</a></li>
+                        <li><a href="">Payments</a></li>
+                        <li><a href="">All Use Cases</a></li>
+                      </ul>
+                    </div>
+                  </div>
+                } />
+
               <a href="#" className='px-3 md:px-4 lg:px-5 text-neutral-600'>Pricing</a>
             </div>
 
@@ -83,14 +147,7 @@ const Navbar = () => {
 
             <AnimatePresence>
               {navOpened && (
-                <motion.div
-                  initial={{ x: "-100vw" }}
-                  animate={{ x: 0 }}
-                  exit={{ x: "-100vw" }}
-                  transition={{ type: 'just', duration: .2 }}
-                  className="bg-white h-[calc(100vh-75px)] w-full absolute top-[75px] left-0" >
-                  Navbar
-                </motion.div>
+                <Mobile />
               )}
             </AnimatePresence>
           </div>
@@ -116,11 +173,130 @@ const NavLink = ({ hoverComponent, title }) => {
         className='cursor-pointer px-3 md:px-4 lg:px-5 text-neutral-600'>{title}</motion.a>
 
       {
-        isLinkHovered && <motion.div className='absolute bg-white border-solid border-[1px] border-neutral-300 rounded-lg p-3 pt-4 top-8 w-auto' layoutId="card">
+        (isLinkHovered || isCardHovered) && <motion.div
+          onHoverStart={(e) => setIsCardHovered(true)}
+          onHoverEnd={(e) => setIsCardHovered(false)} className='absolute bg-white border-solid border-[1px] border-neutral-300 rounded-lg p-3 pt-4 w-auto' layoutId="card">
           {hoverComponent}
         </motion.div>
       }
     </div>
+  )
+}
+
+const Mobile = () => {
+  const [openedLink, setOpenedLink] = useState('')
+
+  return (
+    <motion.div
+      initial={{ x: "-100vw" }}
+      animate={{ x: 0 }}
+      exit={{ x: "-100vw" }}
+      transition={{ type: 'just', duration: .2 }}
+      className="bg-slate-800 min-h-[calc(100vh-75px)] h-auto w-[80%] shadow-2xl absolute px-4 py-4 top-[75px] left-0" >
+      <div className='my-3'>
+        <button className={`text-lg px-2  py-1 flex items-center justify-between ${poppins.className} font-medium w-full text-white`} onClick={() => setOpenedLink((openedLink === "Company" ? "" : "Company"))}>
+          <span>Company</span>
+          <i className={`fa-solid fa-${openedLink === "Company" ? "chevron-up" : "chevron-down"} text-green-300 ml-2`}></i>
+        </button>
+
+        <motion.div
+          className='mobile__nav__category overflow-hidden pl-4'
+          style={{ height: openedLink === "Company" ? "auto" : "0px" }}>
+          <div>
+            <h4 className='text-green-300 text-lg mt-3 mb-2'>About</h4>
+            <ul className={`pl-6 pr-3 flex flex-col gap-y-2 text-slate-400`}>
+              <li className='hover:text-slate-200'><a href="#">Who We Are</a></li>
+              <li className='hover:text-slate-200'><a href="#">Customer Stories</a></li>
+              <li className='hover:text-slate-200'><a href="#">Blog</a></li>
+              <li className='hover:text-slate-200'><a href="#">Careeres</a></li>
+              <li className='hover:text-slate-200'><a href="#">Leadership</a></li>
+              <li className='hover:text-slate-200'><a href="#">Investors</a></li>
+            </ul>
+          </div>
+          <div>
+            <h4 className='text-green-300 text-lg mt-3 mb-2'>Contact</h4>
+            <ul className={`pl-6 pr-3 flex flex-col gap-y-2 text-slate-400`}>
+              <li className='hover:text-slate-200'><a href="#">Consulting</a></li>
+              <li className='hover:text-slate-200'><a href="#">Training</a></li>
+              <li className='hover:text-slate-200'><a href="#">Customer Support</a></li>
+              <li className='hover:text-slate-200'><a href="#">Customer Success</a></li>
+            </ul>
+          </div>
+          <div>
+            <h4 className='text-green-300 text-lg mt-3 mb-2'>Partnerships</h4>
+            <ul className={`pl-6 pr-3 flex flex-col gap-y-2 text-slate-400`}>
+              <li className='hover:text-slate-200'><a href="#">Become a Partner</a></li>
+              <li className='hover:text-slate-200'><a href="#">Find a Partner</a></li>
+              <li className='hover:text-slate-200'><a href="#">Digio for Startups</a></li>
+            </ul>
+          </div>
+        </motion.div>
+      </div>
+      <div className='my-6'>
+        <button className={`text-lg px-2 py-1 items-center ${poppins.className} font-medium w-full flex justify-between text-white`} onClick={() => setOpenedLink((openedLink === "Services" ? "" : "Services"))}>
+          <span>Services</span>
+          <i className={`fa-solid fa-${openedLink === "Services" ? "chevron-up" : "chevron-down"} text-green-300`}></i>
+        </button>
+
+        <motion.div
+          className='mobile__nav__category overflow-hidden pl-4'
+          style={{ height: openedLink === "Services" ? "auto" : "0px" }}>
+          <div>
+            <h4 className='text-green-300 text-lg mt-3 mb-2'>Digital Marketing</h4>
+            <ul className={`pl-6 pr-3 flex flex-col gap-y-2 text-slate-400`}>
+              <li className='hover:text-slate-200'><a href="#">SEO</a></li>
+              <li className='hover:text-slate-200'><a href="#">Social Media Management</a></li>
+              <li className='hover:text-slate-200'><a href="#">PPC</a></li>
+              <li className='hover:text-slate-200'><a href="#">Content Marketing</a></li>
+            </ul>
+          </div>
+          <div>
+            <h4 className='text-green-300 text-lg mt-3 mb-2'>Website</h4>
+            <ul className={`pl-6 pr-3 flex flex-col gap-y-2 text-slate-400`}>
+              <li className='hover:text-slate-200'><a href="#">Development</a></li>
+              <li className='hover:text-slate-200'><a href="#">Design</a></li>
+              <li className='hover:text-slate-200'><a href="#">Maintenance</a></li>
+            </ul>
+          </div>
+        </motion.div>
+      </div>
+      <div className='my-6'>
+        <button className={`text-lg px-2 py-1 items-center ${poppins.className} font-medium w-full flex justify-between text-white`} onClick={() => setOpenedLink((openedLink === "Solutions" ? "" : "Solutions"))}>
+          <span>Solutions</span>
+          <i className={`fa-solid fa-${openedLink === "Solutions" ? "chevron-up" : "chevron-down"} text-green-300`}></i>
+        </button>
+
+        <motion.div
+          className='mobile__nav__category overflow-hidden pl-4'
+          style={{ height: openedLink === "Solutions" ? "auto" : "0px" }}>
+          <div>
+            <h4 className='text-green-300 text-lg mt-3 mb-2'>By Industry</h4>
+            <ul className={`pl-6 pr-3 flex flex-col gap-y-2 text-slate-400`}>
+              <li className='hover:text-slate-200'><a href="#">Healthcare</a></li>
+              <li className='hover:text-slate-200'><a href="#">Retail</a></li>
+              <li className='hover:text-slate-200'><a href="#">Manufacturing</a></li>
+              <li className='hover:text-slate-200'><a href="#">All industries</a></li>
+            </ul>
+          </div>
+          <div>
+            <h4 className='text-green-300 text-lg mt-3 mb-2'>By Use Case</h4>
+            <ul className={`pl-6 pr-3 flex flex-col gap-y-2 text-slate-400`}>
+              <li className='hover:text-slate-200'><a href="#">Analytics</a></li>
+              <li className='hover:text-slate-200'><a href="#">Payments</a></li>
+              <li className='hover:text-slate-200'><a href="#">All Use Cases</a></li>
+            </ul>
+          </div>
+        </motion.div>
+        <button className={`text-lg px-2 py-1 ${poppins.className} font-medium my-6 py-1 text-white`}>
+          Company
+        </button>
+
+        <div className={`flex justify-end items-center gap-4 my-6 px-6 ${poppins.className}`}>
+          <button className='px-4 py-2 text-green-300 underline underline-offset-4'>Sign up</button>
+          <button className='bg-green-400 px-6 py-[10px] rounded-md font-medium shadow-lg hover:bg-green-300'>Try Free</button>
+        </div>
+      </div>
+    </motion.div>
   )
 }
 
