62b5ae49015abfa0bdf2cdfd9c4242648567f4b7
Lindrit <lindritsulaj2.0@gmail.com>
2023-03-11 19:45:20 +0100

    About Section Completed

--- sections/About.jsx
+++ sections/About.jsx
@@ -7,13 +7,13 @@ const About = () => {
   const [selectedTab, setSelectedTab] = useState('Our Work Process')
 
   return (
-    <section className="about bg-darkBackground">
-      <div className='container mx-auto py-10'>
-        <h2 className={`${playfair.className} text-5xl text-white font-medium tracking-wide`}>Innovative Solutions for Your Online Presence</h2>
-        <h3 className='text-neutral-400 text-2xl font-medium mt-6'>We deliver high quality, responsive and fast websites.</h3>
+    <section className="about py-10">
+      <div className='container mx-auto py-10 px-4'>
+        <h2 className={`${playfair.className} text-3xl md:text-4xl lg:text-5xl text-white font-medium tracking-wide`}>Innovative Solutions for Your Online Presence</h2>
+        <h3 className='text-neutral-400 text-lg md:text-xl lg:text-2xl font-medium mt-6'>We deliver high quality, responsive and fast websites.</h3>
 
-        <div className='mt-16'>
-          <div className='flex flex-col gap-10'>
+        <div className='flex flex-wrap justify-center lg:justify-between mt-10 lg:mt-0 items-center lg:flex-nowrap'>
+          <div className='flex flex-col w-full lg:w-1/2 gap-10'>
             <LayoutGroup>
               <Tab
                 title="Who We Are"
@@ -32,7 +32,7 @@ const About = () => {
                 setSelectedTab={setSelectedTab} />
             </LayoutGroup>
           </div>
-          <img src="" alt="" />
+          <img className='w-[80%] h-auto lg:w-[40%]' src="https://ik.imagekit.io/0s9lwb2yr/Digio/2965738-removebg_IJKrAku5M.png?updatedAt=1678558641713" alt="" />
         </div>
       </div>
     </section>
@@ -48,7 +48,7 @@ const Tab = ({ title, description, selectedTab, setSelectedTab }) => {
     <div
       className={`border-solid border-l-[3px] ${isSelected ? 'border-l-blue-500' : 'border-l-neutral-500'} px-8 py-4`}>
       <h3
-        className={`text-[22px] text-white font-bold cursor-pointer ${isSelected && "mb-4"}`}
+        className={`text-lg md:text-[22px] text-white font-bold cursor-pointer ${isSelected && "mb-4"}`}
         onClick={() => isSelected ? deselect() : select()}>{title}</h3>
 
       <AnimatePresence>
