25b938abfe0a2fd4def45df48a4c85630931919f
Lindrit <lindritsulaj2.0@gmail.com>
2023-03-20 16:29:27 +0100

    Replaced metadata with old head.js file

---
+++ app/head.js
@@ -0,0 +1,8 @@
+export default function Head() {
+  return (
+    <>
+      <title>Digio | Grow your business today</title>
+      <meta name="description" content="Digio is a leading web development and digital marketing company that helps businesses grow their online presence. With a team of experts in web design, development, SEO, PPC, and social media marketing, Digio delivers customized solutions that drive results. Contact us today to learn how we can help you achieve your online goals." />
+    </>
+  )
+}
--- app/layout.js
+++ app/layout.js
@@ -21,10 +21,10 @@ export const playfair = Playfair_Display({
   display: 'swap',
 })
 
-export const metadata = {
-  title: 'Digio | Grow your business today',
-  description: 'Digio is a leading web development and digital marketing company that helps businesses grow their online presence. With a team of experts in web design, development, SEO, PPC, and social media marketing, Digio delivers customized solutions that drive results. Contact us today to learn how we can help you achieve your online goals.',
-}
+// export const metadata = {
+//   title: 'Digio | Grow your business today',
+//   description: 'Digio is a leading web development and digital marketing company that helps businesses grow their online presence. With a team of experts in web design, development, SEO, PPC, and social media marketing, Digio delivers customized solutions that drive results. Contact us today to learn how we can help you achieve your online goals.',
+// }
 
 export default function RootLayout({ children }) {
   return (
